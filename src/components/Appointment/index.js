41553afeaf9a4360dd289c2fa3408b688361997b
Ian <ianlam1221@hotmail.com>
2021-02-17 20:05:52 -0800

    added custom hook to transition to different modes

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,19 +5,41 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+
+import { useVisualMode } from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY"; 
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
   <article className="appointment">
     <Header 
       time = {props.time}
     /> 
-    {props.interview ? 
-    <Show 
-      student = {props.interview.student}
-      interviewer = {props.interview.interviewer}
-    /> : <Empty />}
+  {mode === EMPTY && <Empty onAdd={() => transition("CREATE")} />}
+
+  {mode === SHOW && (
+    <Show
+      student={props.interview.student}
+      interviewer={props.interview.interviewer}
+    />
+  )}
+
+  {mode === CREATE && (
+    <Form 
+      interviewers={props.interviewers}
+      onSave={() => console.log("Clicked onSave")}
+      onCancel={() => back()}
+    /> 
+  )}
   </article>
   ); 
 }
