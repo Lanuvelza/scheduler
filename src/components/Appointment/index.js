4054f0cd63e2c9ff70713732d8355c7beff86dbc
Ian <ianlam1221@hotmail.com>
2021-02-18 12:59:29 -0800

    added a save function from Form component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,18 +6,30 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status"; 
 
 import { useVisualMode } from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY"; 
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING"
 
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING)
+    props.bookInterview(props.id, interview)
+    .then(() => transition(SHOW));
+  }
 
   return (
   <article className="appointment">
@@ -36,8 +48,14 @@ export default function Appointment(props) {
   {mode === CREATE && (
     <Form 
       interviewers={props.interviewers}
-      onSave={() => console.log("Clicked onSave")}
+      onSave={save}
       onCancel={() => back()}
+    /> 
+  )}
+
+  {mode === SAVING && (
+    <Status 
+      message="Saving"
     /> 
   )}
   </article>
