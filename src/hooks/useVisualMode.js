53938eabde7aaa0e110091509820019189d6f9c0
Ian <ianlam1221@hotmail.com>
2021-02-19 11:45:52 -0800

    refactored to use previous state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,19 +4,28 @@ export function useVisualMode(initial) {
   const [mode, setMode] = useState(initial)
   const [history, setHistory] = useState([initial]);
 
+  // transitions to the next mode and adds to history if replace boolean is false
   function transition(newMode, replace) {
     if (!replace) {
-      setHistory([...history, mode]);
+      setHistory(prev => {
+        return [...prev, newMode]
+      }); 
     }
     setMode(newMode);
   }
 
+  // goes back to the previous mode in history if history length > 1
   function back() {
-    if (history.length >= 1) {
-      setMode(history.pop()); 
+    if (history.length > 1) {
+      setHistory(prev => {
+        const newHistory = [...prev];
+        newHistory.pop();
+        setMode(newHistory[newHistory.length - 1]);
+        return newHistory
+      });
     }
   }
-
+  
   return { 
     mode, 
     transition,
