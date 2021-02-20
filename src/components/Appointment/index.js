d91d2a3a97b2616aaaac73db0cd7428ae8c7b489
Ian <ianlam1221@hotmail.com>
2021-02-19 18:38:47 -0800

    included a useEffect to check for changes from another user

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React from "react"; 
+import React, { useEffect } from "react"; 
 
 import "./styles.scss";
 
@@ -27,6 +27,15 @@ export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  useEffect(() => {
+    if (props.interview && mode === EMPTY) {
+      transition(SHOW);
+    }
+    if (props.interview === null && mode === SHOW) {
+      transition(EMPTY); 
+    }
+  }, [props.interview, transition, mode]);
 
   function save(name, interviewer) {
     const interview = {
@@ -60,7 +69,7 @@ export default function Appointment(props) {
     />
   )}
 
-  {mode === SHOW && (
+  {mode === SHOW && props.interview && (
     <Show
       student={props.interview.student}
       interviewer={props.interview.interviewer}
