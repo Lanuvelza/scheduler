4be22f82933c9f4eebc9c6fc1e49af7662fa1abb
Ian <ianlam1221@hotmail.com>
2021-02-24 12:18:01 -0800

    deploying on netlify

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 import "index.scss";
 
-import axios from "axios"; 
+import axios from "axios";
 
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
