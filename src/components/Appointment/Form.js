e7413eb8e88b14ea736e186ab4825e5095285549
Ian <ianlam1221@hotmail.com>
2021-02-24 16:20:29 -0800

    checks to see if the interviewer is also selected in validate function

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -23,6 +23,12 @@ export default function Form(props) {
      setError("Student name cannot be blank");
      return;
    }
+
+   if (!interviewer) {
+     setError("Please select an interviewer"); 
+     return;
+   }
+   
    setError("");
    props.onSave(name, interviewer);
  };
